86d72d356de5e759476ef970b2a090dc4c966acb
Enrique Cornejo <enrique@cornejo.me>
2017-12-11 11:46:04 +0100

    First commit

---
+++ base/prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+unset XDG_RUNTIME_DIR
+set -e
+set -x
+
+if [ -e "/opt/app/environment.yml" ]; then
+    echo "environment.yml found. Installing packages"
+    /opt/conda/bin/conda env update -n dask -f /opt/app/environment.yml
+else
+    echo "no environment.yml"
+fi
